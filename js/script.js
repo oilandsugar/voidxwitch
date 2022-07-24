ce14b42303e089e48ef93c80647c82c6f8089750
ritualdust <lizbeth.porier.m@gmail.com>
2022-07-24 11:51:43 -0400

    add video teaser and changing backgrounds

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const setBackgroundImage = () => {
+  let images = [
+    "img/random-background/are-you-ok-1.jpg",
+    "img/random-background/are-you-ok-2.jpg",
+    "img/random-background/are-you-ok-3.jpg",
+    "img/random-background/are-you-ok-4.jpg",
+    "img/random-background/are-you-ok-5.jpg",
+    "img/random-background/are-you-ok-6.jpg",
+    "img/random-background/are-you-ok-7.jpg",
+    "img/random-background/are-you-ok-8.jpg",
+    "img/random-background/are-you-ok-9.jpg",
+    "img/random-background/are-you-ok-10.jpg",
+    "img/random-background/are-you-ok-11.jpg",
+    "img/random-background/are-you-ok-12.jpg",
+    "img/random-background/are-you-ok-13.jpg",
+  ]
+  let randSelect = images[Math.floor(Math.random() * images.length)]
+  document.body.style.backgroundImage = 'url(' + randSelect + ')';
+}
+
+setBackgroundImage();
