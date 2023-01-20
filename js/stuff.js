f3cd5c9733bf48865b123ea2ad1349da0fbbc27c
ritualdust <lizbeth.porier.m@gmail.com>
2023-01-20 16:22:32 -0500

    add image road

--- js/stuff.js
+++ js/stuff.js
@@ -1,3 +1,23 @@
+// slider
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+
+nextButton.addEventListener("click", (event) => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
+
+// random background image
+
 const setBackgroundImage = () => {
   let images = [
     "img/random-background/background-1.jpg",
